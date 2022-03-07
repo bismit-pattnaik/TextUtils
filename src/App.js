9d4dac9471be657693cfe272d803f06c5b9d5ac1
bismit-pattnaik <bismit77@gmail.com>
2022-03-08 02:56:38 +0530

    switch mode l12(h)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 //import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import React, { useState } from 'react'
 
 function App() {
+
+ const [mode, setmode] = useState('dark')
+
+ const toggleMode = ()=>{
+   if(mode==='dark')
+   setmode('light');
+
+   else
+    setmode('dark');
+ }
+
   return (
   <>
-     {/* <Navbar/> */} 
-    <Navbar title="TextUtils" AboutText='About us' />
+     {/* <Navbar/> 
+    <Navbar title="TextUtils" AboutText='About us' /> */}
 
-    <div className="container my-3">
-      <Textform heading= "Enter your text below" />
-      {/* <About/> */}
-    </div>
+    <Navbar title='TextUtilss' mode={mode} toggleMode={toggleMode}/>
+
+
+      <div className="container my-3">
+       <Textform heading= "Enter your text below" />
+            {/* <About/> */}
+       </div>
     
   </>
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/">{ props.title }</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,13 +17,17 @@ export default function Navbar(props) {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/"> {props.AboutText}</a>
+                <a className="nav-link" href="/"> About</a>
               </li>
             </ul>
-            <form className="d-flex">
+            {/* <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button className="btn btn-primary" type="submit">Search</button>
-            </form>
+            </form> */}
+              <div class={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                <input class="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                <label class="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
+              </div>
           </div>
         </div>
       </nav>
